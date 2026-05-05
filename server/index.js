91e9692051d7c7f4861a368017f0b2e919791fe8
Sathwika527 <sathwikarudravena141@gmail.com>
2026-05-05 10:52:19 +0530

    final deploy

--- server/index.js
+++ server/index.js
@@ -31,5 +31,5 @@ app.delete("/tasks/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
