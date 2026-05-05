8e82c49445a74b40e7934d1dfcd06f9a066c6168
Sathwika527 <sathwikarudravena141@gmail.com>
2026-05-05 06:39:30 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let students = [];
+
+// GET
+app.get("/students", (req, res) => {
+  res.json(students);
+});
+
+// POST
+app.post("/students", (req, res) => {
+  students.push({ ...req.body, id: Date.now() });
+  res.json({ message: "Added" });
+});
+
+// DELETE
+app.delete("/students/:id", (req, res) => {
+  students = students.filter(s => s.id != req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on", PORT));
